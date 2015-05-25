bb5ec25e8deb1234c009f7fe010ebc377be78e23
Adrian Freemantle <adrian@synerics.com>
2015-05-25 08:12:41 +0200

    restructuring code base

---
+++ server/Src/MobilePoll.MessageContracts/Events/MultiOptionReceived.cs
@@ -0,0 +1,11 @@
+﻿using MobilePoll.Bus;
+
+namespace MobilePoll.MessageContracts.Events
+{
+    public class MultiOptionReceived : IEvent
+    {
+        public string[] Result { get; set; }
+        public int QuestionId { get; set; }
+        public string Question { get; set; }
+    }
+}
