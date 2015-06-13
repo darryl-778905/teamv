924a0930b4682c76ea74b4a64d5ae14658199aa8
Adrian Freemantle <adrian@synerics.com>
2015-06-13 14:28:15 +0200

    added reports done by Kyle. Added SurveyGenerator as build by Chris and Derrick

---
+++ SurveyGenerator/Models/Survey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurveyGenerator.Models
+{
+    public class Survey
+    {
+        public string SurveyName { get; set; }
+    }
+    public enum Gender
+    {
+        Male =0,
+        FeMale = 1
+    }
+}
